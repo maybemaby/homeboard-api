25ba197b5dcdbe692f09c8050bb7e9d56df00d78
maybemaby <74226064+maybemaby@users.noreply.github.com>
2022-02-16 09:48:55 -0800

    include roommate data in homeService queries

--- src/services/HomeService.ts
+++ src/services/HomeService.ts
@@ -2,13 +2,20 @@ import prisma from "../data/db";
 import { IHome } from "../models/Home";
 
 async function getAll() {
-  return prisma.home.findMany();
+  return prisma.home.findMany({
+    include: {
+      roommates: true,
+    },
+  });
 }
 
 async function getById(id: string) {
   const home = await prisma.home.findUnique({
     where: {
       id: id,
+    },
+    include: {
+      roommates: true,
     },
   });
   return home;
