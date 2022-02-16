50f27fb5529284140d875d2293b3740d1a176d4c
maybemaby <74226064+maybemaby@users.noreply.github.com>
2022-02-16 09:48:10 -0800

    add userRouter to api

--- src/routers/api/index.ts
+++ src/routers/api/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { homeRouter } from "./homeRouter";
+import { userRouter } from "./userRouter";
 
 export const apiRouter = Router();
 
 apiRouter.use("/v1/homes", homeRouter);
+apiRouter.use("/v1/user", userRouter);
